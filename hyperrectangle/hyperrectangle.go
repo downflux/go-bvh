4861054ad295f720f20f9d00174b29dfda0682ff
Minke Zhang <minke.zhang@gmail.com>
2022-08-09 23:57:00 -0700

    Add hyperrectangle.Bound to create a bounding box for a collection of points

--- hyperrectangle/hyperrectangle.go
+++ hyperrectangle/hyperrectangle.go
@@ -3,9 +3,56 @@ package hyperrectangle
 import (
 	"math"
 
+	"github.com/downflux/go-bvh/point"
 	"github.com/downflux/go-geometry/nd/hyperrectangle"
 	"github.com/downflux/go-geometry/nd/vector"
 )
+
+type BoundOpt[T point.P] struct {
+	Data []T
+	K    vector.D
+	Size int
+}
+
+func Bound[T point.P](o BoundOpt[T]) hyperrectangle.R {
+	min := make([]float64, o.K)
+	max := make([]float64, o.K)
+
+	for i := vector.D(0); i < o.K; i++ {
+		min[i] = math.Inf(0)
+		max[i] = math.Inf(-1)
+	}
+
+	b := *hyperrectangle.New(vector.V(min), vector.V(max))
+
+	if len(o.Data) < o.Size {
+		for _, p := range o.Data {
+			b = Union(b, p.Bound())
+		}
+	} else {
+		l := make(chan hyperrectangle.R)
+		r := make(chan hyperrectangle.R)
+		go func(ch chan<- hyperrectangle.R) {
+			ch <- Bound[T](BoundOpt[T]{
+				Data: o.Data[0 : len(o.Data)/2],
+				K:    o.K,
+				Size: o.Size,
+			})
+			close(ch)
+		}(l)
+		go func(ch chan<- hyperrectangle.R) {
+			ch <- Bound[T](BoundOpt[T]{
+				Data: o.Data[len(o.Data)/2 : len(o.Data)-1],
+				K:    o.K,
+				Size: o.Size,
+			})
+			close(ch)
+		}(r)
+		b = Union(<-l, <-r)
+	}
+
+	return b
+}
 
 func Collision(r hyperrectangle.R, s hyperrectangle.R) bool {
 	if r.Min().Dimension() != s.Min().Dimension() {
