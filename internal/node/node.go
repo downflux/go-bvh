b48d8d04dc78945e1e999a3b0d01479c2ece60ab
Minke Zhang <minke.zhang@gmail.com>
2022-08-14 16:40:37 -0700

    Add node.Parent API

--- internal/node/node.go
+++ internal/node/node.go
@@ -19,13 +19,14 @@ type N[T point.P] struct {
 func (n *N[T]) B() hyperrectangle.R { return n.bounds }
 func (n *N[T]) Leaf() bool          { return n.left == nil && n.right == nil }
 
-func (n *N[T]) Data() T  { return n.data }
-func (n *N[T]) L() *N[T] { return n.left }
-func (n *N[T]) R() *N[T] { return n.right }
+func (n *N[T]) Data() T       { return n.data }
+func (n *N[T]) L() *N[T]      { return n.left }
+func (n *N[T]) R() *N[T]      { return n.right }
+func (n *N[T]) Parent() *N[T] { return n.parent }
 
 func (n *N[T]) Move(q hyperrectangle.R, f filter.F[T], dp vector.V) {}
-func (n *N[T]) Insert(p T)                                          {}
 func (n *N[T]) Remove(q hyperrectangle.R, f filter.F[T]) (T, bool) {
 	var blank T
 	return blank, false
 }
+func (n *N[T]) Insert(p T) {}
