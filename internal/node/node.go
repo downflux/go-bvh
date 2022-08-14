d47cfb0d76a3a63d281efa3ae011eaaf727f0529
Minke Zhang <mzhang@blogzhang.com>
2022-08-14 15:27:58 -0700

    Add filter type and additional node API endpoints

--- internal/node/node.go
+++ internal/node/node.go
@@ -1,12 +1,16 @@
 package node
 
 import (
+	"github.com/downflux/go-bvh/filter"
 	"github.com/downflux/go-bvh/point"
 	"github.com/downflux/go-geometry/nd/hyperrectangle"
+	"github.com/downflux/go-geometry/nd/vector"
 )
 
 type N[T point.P] struct {
-	data   T
+	data T
+
+	parent *N[T]
 	left   *N[T]
 	right  *N[T]
 	bounds hyperrectangle.R
@@ -18,3 +22,10 @@ func (n *N[T]) Leaf() bool          { return n.left == nil && n.right == nil }
 func (n *N[T]) Data() T  { return n.data }
 func (n *N[T]) L() *N[T] { return n.left }
 func (n *N[T]) R() *N[T] { return n.right }
+
+func (n *N[T]) Move(q hyperrectangle.R, f filter.F[T], dp vector.V) {}
+func (n *N[T]) Insert(p T)                                          {}
+func (n *N[T]) Remove(q hyperrectangle.R, f filter.F[T]) (T, bool) {
+	var blank T
+	return blank, false
+}
