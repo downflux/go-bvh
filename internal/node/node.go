67112aade66492ca0f0638930a7efecec31d6046
Minke Zhang <minke.zhang@gmail.com>
2022-08-15 21:39:49 -0700

    Update node and point API

--- internal/node/node.go
+++ internal/node/node.go
@@ -7,21 +7,21 @@ import (
 )
 
 type N struct {
-	id point.ID
+	ids []point.ID
 
 	parent *N
 	left   *N
 	right  *N
-	bound hyperrectangle.R
+	bound  hyperrectangle.R
 }
 
 func (n *N) Bound() hyperrectangle.R { return n.bound }
-func (n *N) Leaf() bool          { return n.left == nil && n.right == nil }
+func (n *N) Leaf() bool              { return n.left == nil && n.right == nil }
 
-func (n *N) ID() point.ID { return n.id }
-func (n *N) L() *N        { return n.left }
-func (n *N) R() *N        { return n.right }
-func (n *N) Parent() *N   { return n.parent }
+func (n *N) IDs() []point.ID { return n.ids }
+func (n *N) L() *N           { return n.left }
+func (n *N) R() *N           { return n.right }
+func (n *N) Parent() *N      { return n.parent }
 
 func (n *N) Move(id point.ID, offset vector.V) bool { return false }
 func (n *N) Remove(id point.ID) bool                { return false }
