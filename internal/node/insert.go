a54fef73f7e66008bcfc5353572becb838855c97
Minke Zhang <minke.zhang@gmail.com>
2022-08-16 03:05:07 -0700

    Refactor insert op

--- internal/node/insert.go
+++ internal/node/insert.go
@@ -12,51 +12,51 @@ import (
 func Insert(root *N, id point.ID, bound hyperrectangle.R) {
 	// Find best new sibling for the new leaf.
 	q := pq.New[*N](0)
-	q.Push(root, -float64(heuristic.Heuristic(bhr.Union(root.Bound, bound))))
+	q.Push(root, -float64(heuristic.Heuristic(bhr.Union(root.Bound(), bound))))
 
 	candidate := root
 
 	for n := q.Pop(); n != nil || !q.Empty(); n = q.Pop() {
-		direct := heuristic.Heuristic(bhr.Union(bound, n.Bound))
+		direct := heuristic.Heuristic(bhr.Union(bound, n.Bound()))
 
 		inherited := heuristic.H(0.0)
-		for m := n.Parent; m.Parent != nil; m = m.Parent {
-			inherited += heuristic.Heuristic(bhr.Union(bound, m.Bound)) - heuristic.Heuristic(bound)
+		for m := n.Parent(); m.Parent() != nil; m = m.Parent() {
+			inherited += heuristic.Heuristic(bhr.Union(bound, m.Bound())) - heuristic.Heuristic(bound)
 		}
 
 		if float64(direct+inherited) < -q.Priority() {
 			candidate = n
 		}
 
-		h := heuristic.Heuristic(bound) + (heuristic.Heuristic(bhr.Union(bound, n.Bound)) - heuristic.Heuristic(n.Bound))
+		h := heuristic.Heuristic(bound) + (heuristic.Heuristic(bhr.Union(bound, n.Bound())) - heuristic.Heuristic(n.Bound()))
 		if float64(h) < -q.Priority() {
-			q.Push(n.Left, -float64(h))
-			q.Push(n.Right, -float64(h))
+			q.Push(n.Left(), -float64(h))
+			q.Push(n.Right(), -float64(h))
 		}
 	}
 
 	// Create new parent.
-	parent := &N{
-		Parent: candidate.Parent,
-		Left: &N{
+	parent := New(O{
+		Parent: candidate.Parent(),
+		Left: New(O{
 			ID:    id,
 			Bound: bound,
-		},
+		}),
 		Right: candidate,
-		Bound: bhr.Union(bound, candidate.Bound),
-	}
-	if candidate.Parent != nil {
-		if candidate.Parent.Left == candidate {
-			candidate.Parent.Left = parent
+		Bound: bhr.Union(bound, candidate.Bound()),
+	})
+	if candidate.Parent() != nil {
+		if candidate.Parent().Left() == candidate {
+			candidate.Parent().left = parent
 		} else {
-			candidate.Parent.Right = parent
+			candidate.Parent().right = parent
 		}
 	}
-	candidate.Parent = parent
-	parent.Left.Parent = parent
+	candidate.parent = parent
+	parent.Left().parent = parent
 
 	// Refit AABBs.
-	for m := parent.Parent; m.Parent != nil; m = m.Parent {
-		m.Bound = bhr.Union(bound, m.Bound)
+	for m := parent.Parent(); m.Parent() != nil; m = m.Parent() {
+		m.bound = bhr.Union(bound, m.Bound())
 	}
 }
--- internal/node/node.go
+++ internal/node/node.go
@@ -6,7 +6,7 @@ import (
 	"github.com/downflux/go-geometry/nd/vector"
 )
 
-type N struct {
+type O struct {
 	ID     point.ID
 	Parent *N
 	Left   *N
@@ -15,7 +15,31 @@ type N struct {
 	Bound hyperrectangle.R
 }
 
-func (n *N) Leaf() bool { return n.Left == nil && n.Right == nil }
+type N struct {
+	id     point.ID
+	parent *N
+	left   *N
+	right  *N
+	bound  hyperrectangle.R
+}
+
+func New(o O) *N {
+	return &N{
+		id:     o.ID,
+		parent: o.Parent,
+		left:   o.Left,
+		right:  o.Right,
+		bound:  o.Bound,
+	}
+}
+
+func (n *N) Bound() hyperrectangle.R { return n.bound }
+
+func (n *N) Leaf() bool   { return n.Left() == nil && n.Right() == nil }
+func (n *N) Left() *N     { return n.left }
+func (n *N) Right() *N    { return n.right }
+func (n *N) Parent() *N   { return n.parent }
+func (n *N) ID() point.ID { return n.id }
 
 func (n *N) Move(id point.ID, offset vector.V) bool { return false }
 func (n *N) Remove(id point.ID) bool                { return false }
