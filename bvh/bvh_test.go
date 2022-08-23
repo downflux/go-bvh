4836f6a12ef82f7a5491356adb40e4fd40c73558
Minke Zhang <minke.zhang@gmail.com>
2022-08-22 21:50:42 -0700

    Set up BVH test framewrk

--- bvh/bvh_test.go
+++ bvh/bvh_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/downflux/go-bvh/internal/node/util"
 	"github.com/downflux/go-bvh/point"
 	"github.com/downflux/go-geometry/nd/hyperrectangle"
+	"github.com/google/go-cmp/cmp"
 )
 
 type n struct {
@@ -15,7 +16,7 @@ type n struct {
 func (n n) Bound() hyperrectangle.R { return hyperrectangle.R{} }
 func (n n) ID() point.ID            { return n.id }
 
-func Equal(a, b BVH[*n]) bool {
+func Equal(a, b *BVH[*n]) bool {
 	if !util.Equal(a.allocation, a.root, b.allocation, b.root) {
 		return false
 	}
@@ -35,5 +36,26 @@ func Equal(a, b BVH[*n]) bool {
 }
 
 func TestRemove(t *testing.T) {
+	type config struct {
+		name string
+		bvh  *BVH[*n]
+		i    point.ID
+		want *BVH[*n]
+	}
 
+	configs := []config{}
+
+	for _, c := range configs {
+		t.Run(c.name, func(t *testing.T) {
+			c.bvh.Remove(c.i)
+			if diff := cmp.Diff(
+				c.want,
+				c.bvh,
+				cmp.Comparer(func(a, b *BVH[*n]) bool {
+					return Equal(a, b)
+				})); diff != "" {
+				t.Errorf("Remove() mismatch (-want +got):\n%v", diff)
+			}
+		})
+	}
 }
