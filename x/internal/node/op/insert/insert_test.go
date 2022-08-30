f4107afc40a6c41cff493050e155b60f8fe55d5d
Minke Zhang <minke.zhang@gmail.com>
2022-08-30 00:41:02 -0700

    Add more parent tests

--- x/internal/node/op/insert/insert_test.go
+++ x/internal/node/op/insert/insert_test.go
@@ -68,14 +68,56 @@ func TestParent(t *testing.T) {
 				Root: 100,
 			}),
 		},
+		func() config {
+			data := map[util.NodeID]map[id.ID]hyperrectangle.R{
+				101: {1: util.Interval(0, 100)},   // w
+				102: {2: util.Interval(101, 200)}, // x
+				103: {3: util.Interval(201, 300)}, // y
+				104: {4: util.Interval(301, 400)}, // z
+			}
+			return config{
+				name: "SwapInternal",
+				//   A
+				//  / \
+				// w   C
+				//    / \
+				//   x   y
+				n: util.New(util.T{
+					Data: data,
+					Nodes: map[util.NodeID]util.N{
+						100: {Left: 101, Right: 105}, // A
+						105: {Left: 102, Right: 103}, // C
+					},
+					Root: 100,
+				}).Right(), // C
+				pid:  4, // z
+				aabb: data[104][4],
+				//   A
+				//  / \
+				// w   D
+				//    / \
+				//   z   C
+				//      / \
+				//     x   y
+				want: util.New(util.T{
+					Data: data,
+					Nodes: map[util.NodeID]util.N{
+						100: {Left: 101, Right: 105}, // A
+						105: {Left: 104, Right: 106}, // D
+						106: {Left: 102, Right: 103}, // C
+					},
+					Root: 100,
+				}).Right(), // D
+			}
+		}(),
 	}
 
 	for _, c := range configs {
 		t.Run(c.name, func(t *testing.T) {
 			got := parent(c.n, c.pid, c.aabb)
 			if diff := cmp.Diff(
-				c.want,
-				got,
+				c.want.Root(),
+				got.Root(),
 				cmp.Comparer(util.Equal),
 			); diff != "" {
 				t.Errorf("parent() mismatch (-want +got):\n%v", diff)
