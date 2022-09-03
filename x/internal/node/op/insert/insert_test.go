4ffddd9f1318df36756ced7a0c039d0460a4018b
Minke Zhang <minke.zhang@gmail.com>
2022-09-03 03:19:01 -0700

    Add insert.sibling test

--- x/internal/node/op/insert/insert_test.go
+++ x/internal/node/op/insert/insert_test.go
@@ -12,6 +12,27 @@ import (
 	nid "github.com/downflux/go-bvh/x/internal/node/id"
 )
 
+func TestExecute(t *testing.T) {
+	type config struct {
+		name string
+		root *node.N
+		id   id.ID
+		aabb hyperrectangle.R
+		want *node.N
+	}
+
+	configs := []config{}
+
+	for _, c := range configs {
+		t.Run(c.name, func(t *testing.T) {
+			got := Execute(c.root, c.id, c.aabb)
+			if diff := cmp.Diff(c.want, got, cmp.Comparer(util.Equal)); diff != "" {
+				t.Errorf("Execute() mismatch (-want +got):\n%v", diff)
+			}
+		})
+	}
+}
+
 func TestSibling(t *testing.T) {
 	type config struct {
 		name string
