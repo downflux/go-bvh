55b364bc0e534c37fd17f1ecfe46c3b01b7d74a5
Minke Zhang <minke.zhang@gmail.com>
2022-09-03 23:44:45 -0700

    Add BVH BroadPhase test

--- x/bvh/bvh_test.go
+++ x/bvh/bvh_test.go
@@ -8,9 +8,79 @@ import (
 	"github.com/downflux/go-bvh/x/internal/node/util"
 	"github.com/downflux/go-geometry/nd/hyperrectangle"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 
 	nid "github.com/downflux/go-bvh/x/internal/node/id"
 )
+
+func TestBroadPhase(t *testing.T) {
+	type config struct {
+		name string
+		bvh  *BVH
+		q    hyperrectangle.R
+		want []id.ID
+	}
+
+	configs := []config{
+		func() config {
+			root := util.New(util.T{
+				Data: map[nid.ID]map[id.ID]hyperrectangle.R{
+					100: {1: util.Interval(0, 100)},
+				},
+				Nodes: map[nid.ID]util.N{
+					100: util.N{},
+				},
+				Root: 100,
+			})
+			return config{
+				name: "PartialMatch",
+				bvh: &BVH{
+					lookup: map[id.ID]*node.N{
+						1: root,
+					},
+					root: root,
+				},
+				q:    util.Interval(1, 2),
+				want: []id.ID{1},
+			}
+		}(),
+		func() config {
+			root := util.New(util.T{
+				Data: map[nid.ID]map[id.ID]hyperrectangle.R{
+					100: {1: util.Interval(0, 100)},
+				},
+				Nodes: map[nid.ID]util.N{
+					100: util.N{},
+				},
+				Root: 100,
+			})
+			return config{
+				name: "NoMatch",
+				bvh: &BVH{
+					lookup: map[id.ID]*node.N{
+						1: root,
+					},
+					root: root,
+				},
+				q:    util.Interval(101, 102),
+				want: nil,
+			}
+		}(),
+	}
+
+	for _, c := range configs {
+		t.Run(c.name, func(t *testing.T) {
+			got := BroadPhase(c.bvh, c.q)
+			if diff := cmp.Diff(
+				c.want,
+				got,
+				cmpopts.SortSlices(func(a, b id.ID) bool { return a < b }),
+			); diff != "" {
+				t.Errorf("BroadPhase() mismatch (-want +got):\n%v", diff)
+			}
+		})
+	}
+}
 
 func TestRemove(t *testing.T) {
 	type config struct {
