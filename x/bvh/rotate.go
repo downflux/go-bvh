fd040eee98ec34f1765739e47ddc62b0876d61fa
Minke Zhang <minke.zhang@gmail.com>
2022-11-13 22:29:24 -0800

    Refactor rotate API

--- x/bvh/rotate.go
+++ x/bvh/rotate.go
@@ -18,6 +18,91 @@ const (
 	rotationTypeDF
 )
 
+// r is a mutable rotation struct which keeps track of the current optimal local
+// subtree rotation.
+type r struct {
+	t    rotationType
+	src  node.N
+	dest node.N
+}
+
+func getOptimalRotation(a node.N) *r {
+	r := &r{}
+
+	if a.Height() < 2 {
+		return r
+	}
+
+	b := a.Left()
+	c := a.Right()
+
+	opt := heuristic.H(b.AABB().R()) + heuristic.H(c.AABB().R())
+
+	// Generate a virtual node buffer.
+	k := a.AABB().Min().Dimension()
+	buf := hyperrectangle.New(
+		vector.V(make([]float64, k)),
+		vector.V(make([]float64, k)),
+	).M()
+
+	var d, e, f, g node.N
+	if !c.IsLeaf() {
+		f = c.Left()
+		g = c.Right()
+
+		if h, ok := checkBF(b, f, g, opt, buf); ok {
+			opt = h
+			r.src = b
+			r.dest = f
+			r.t = rotationTypeBF
+		}
+
+		if h, ok := checkBF(b, g, f, opt, buf); ok {
+			opt = h
+			r.src = b
+			r.dest = g
+			r.t = rotationTypeBF
+		}
+	}
+
+	if !b.IsLeaf() {
+		d = b.Left()
+		e = b.Right()
+
+		if h, ok := checkBF(c, d, e, opt, buf); ok {
+			opt = h
+			r.src = c
+			r.dest = d
+			r.t = rotationTypeBF
+		}
+
+		if h, ok := checkBF(c, e, d, opt, buf); ok {
+			opt = h
+			r.src = c
+			r.dest = e
+			r.t = rotationTypeBF
+		}
+	}
+
+	if !b.IsLeaf() && !c.IsLeaf() {
+		if h, ok := checkDF(d, e, f, g, opt, buf); ok {
+			opt = h
+			r.src = d
+			r.dest = f
+			r.t = rotationTypeDF
+
+		}
+		if h, ok := checkDF(d, e, g, f, opt, buf); ok {
+			opt = h
+			r.src = d
+			r.dest = g
+			r.t = rotationTypeDF
+		}
+	}
+
+	return r
+}
+
 // rotate returns a valid rotation which will not unbalance our BVH tree.
 //
 // The list of rotations to be considered is taken from Kopta et al. 2012.
@@ -52,119 +137,49 @@ const (
 //
 //	H(A.L) + H(A.R)
 //
-// to change. WLOG we use the A.L notation here instead of B, as B may have been
-// swapped.
-func rotate(a node.N, data map[id.ID]hyperrectangle.R, epsilon float64) node.N {
-	if a.Height() < 2 {
-		return a
-	}
-
-	b := a.Left()
-	c := a.Right()
-
-	r := &r{}
-
-	// Cache the heuristic values for faster computes.
-	bh := heuristic.H(b.AABB().R())
-	ch := heuristic.H(c.AABB().R())
-
-	opt := bh + ch
-
-	k := a.AABB().Min().Dimension()
-	// Generate some virtual node buffers.
-	bbuf := hyperrectangle.New(
-		vector.V(make([]float64, k)),
-		vector.V(make([]float64, k)),
-	).M()
-	cbuf := hyperrectangle.New(
-		vector.V(make([]float64, k)),
-		vector.V(make([]float64, k)),
-	).M()
-
-	var d, e, f, g node.N
-	if !c.IsLeaf() {
-		f = c.Left()
-		g = c.Right()
-
-		if h, ok := checkBF(b, f, g, opt, bbuf); ok {
-			opt = h
-			r.src = b
-			r.dest = f
-			r.t = rotationTypeBF
-		}
-
-		if h, ok := checkBF(b, g, f, opt, bbuf); ok {
-			opt = h
-			r.src = b
-			r.dest = g
-			r.t = rotationTypeBF
-		}
-	}
-
-	if !b.IsLeaf() {
-		d = b.Left()
-		e = b.Right()
-
-		if h, ok := checkBF(c, d, e, opt, cbuf); ok {
-			opt = h
-			r.src = c
-			r.dest = d
-			r.t = rotationTypeBF
-		}
-
-		if h, ok := checkBF(c, e, d, opt, cbuf); ok {
-			opt = h
-			r.src = c
-			r.dest = e
-			r.t = rotationTypeBF
-		}
-	}
-
-	if !b.IsLeaf() && !c.IsLeaf() {
-		if h, ok := checkDF(d, e, f, g, opt, bbuf, cbuf); ok {
-			opt = h
-			r.src = d
-			r.dest = f
-			r.t = rotationTypeDF
-
-		}
-		if h, ok := checkDF(d, e, g, f, opt, bbuf, cbuf); ok {
-			opt = h
-			r.src = d
-			r.dest = g
-			r.t = rotationTypeDF
-		}
-	}
+// to change.
+func rotate(x node.N, data map[id.ID]hyperrectangle.R, epsilon float64) node.N {
+	r := getOptimalRotation(x)
 
 	switch r.t {
 	case rotationTypeBF:
 		swap(r.src, r.dest)
 
+		// By construction, r.src is the b node, and therefore the
+		// shallower node before the swap. Now that the nodes are
+		// swapped, r.src is the deeper node.
 		node.SetAABB(r.src.Parent(), data, epsilon)
 		node.SetHeight(r.src.Parent())
 
-		node.SetAABB(a, data, epsilon)
-		node.SetHeight(a)
+		node.SetAABB(x, data, epsilon)
+		node.SetHeight(x)
 	case rotationTypeDF:
 		swap(r.src, r.dest)
 
+		// A D -> F rotation means both nodes are of the same depth --
+		// both their parents need to be updated in addition to the
+		// local root node.
 		node.SetAABB(r.src.Parent(), data, epsilon)
 		node.SetHeight(r.src.Parent())
 
 		node.SetAABB(r.dest.Parent(), data, epsilon)
 		node.SetHeight(r.dest.Parent())
 
-		node.SetAABB(a, data, epsilon)
-		node.SetHeight(a)
+		node.SetAABB(x, data, epsilon)
+		node.SetHeight(x)
 	}
 
-	return a
+	return x
 }
 
-type r struct {
-	t    rotationType
-	src  node.N
-	dest node.N
+// merge simulates the results if the input nodes are set as siblings of one
+// another.
+func merge(l node.N, r node.N, buf hyperrectangle.M) (int, bool, float64) {
+	buf.Copy(l.AABB().R())
+	buf.Union(r.AABB().R())
+
+	height := int(math.Abs(float64(l.Height() - r.Height())))
+	return height, height <= 1, heuristic.H(buf.R())
 }
 
 // checkBF checks if a potential B -> F rotation will generate a more efficient
@@ -178,32 +193,21 @@ type r struct {
 //	B   C
 //	   / \
 //	  F   G
-func checkBF(b node.N, f node.N, g node.N, opt float64, bbuf hyperrectangle.M) (float64, bool) {
-	// Simulate a B -> F rotation, which mutates the c node.
-	bbuf.Copy(b.AABB().R())
-	bbuf.Union(g.AABB().R())
-	height := math.Max(float64(b.Height()), float64(g.Height())) + 1
-	balanced := math.Abs(height-float64(f.Height())) <= 1
-
-	h := heuristic.H(f.AABB().R()) + heuristic.H(bbuf.R())
-	if h < opt && balanced {
-		return h, true
+func checkBF(b node.N, f node.N, g node.N, opt float64, buf hyperrectangle.M) (float64, bool) {
+	if _, balanced, h := merge(b, g, buf); balanced && h < opt {
+		return opt, false
 	}
 	return opt, false
 }
 
-func checkDF(d node.N, e node.N, f node.N, g node.N, opt float64, bbuf hyperrectangle.M, cbuf hyperrectangle.M) (float64, bool) {
-	bbuf.Copy(f.AABB().R())
-	bbuf.Union(e.AABB().R())
-	bheight := math.Max(float64(f.Height()), float64(e.Height())) + 1
+func checkDF(d node.N, e node.N, f node.N, g node.N, opt float64, buf hyperrectangle.M) (float64, bool) {
+	bheight, bbalanced, bh := merge(f, e, buf)
+	cheight, cbalanced, ch := merge(d, g, buf)
+	balanced := math.Abs(float64(bheight-cheight)) <= 1
 
-	cbuf.Copy(d.AABB().R())
-	cbuf.Union(g.AABB().R())
-	cheight := math.Max(float64(d.Height()), float64(g.Height())) + 1
-	balanced := math.Abs(bheight-cheight) <= 1
+	h := bh + ch
 
-	h := heuristic.H(bbuf.R()) + heuristic.H(cbuf.R())
-	if h < opt && balanced {
+	if bbalanced && cbalanced && balanced && h < opt {
 		return h, true
 	}
 	return opt, false
