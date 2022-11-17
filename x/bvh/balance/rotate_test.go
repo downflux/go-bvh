466ef98f2717ce843087f002abb5ebf1d99481df
Minke Zhang <minke.zhang@gmail.com>
2022-11-16 23:26:55 -0800

    Add CheckDF tests

--- x/bvh/balance/rotate_test.go
+++ x/bvh/balance/rotate_test.go
@@ -14,6 +14,208 @@ import (
 	cid "github.com/downflux/go-bvh/x/internal/cache/id"
 )
 
+func TestCheckDF(t *testing.T) {
+	const k = 2
+	type w struct {
+		h       float64
+		optimal bool
+	}
+
+	type config struct {
+		name string
+		d    node.N
+		e    node.N
+		f    node.N
+		g    node.N
+		opt  float64
+		want w
+	}
+
+	configs := []config{
+		func() config {
+			c := cache.New(cache.O{
+				LeafSize: 1,
+				K:        k,
+			})
+
+			nd := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nd.AABB().Copy(*hyperrectangle.New(vector.V([]float64{1, 1}), vector.V([]float64{2, 2})))
+
+			ne := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ne.AABB().Copy(*hyperrectangle.New(vector.V([]float64{99, 99}), vector.V([]float64{100, 100})))
+
+			nf := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nf.AABB().Copy(*hyperrectangle.New(vector.V([]float64{97, 97}), vector.V([]float64{98, 98})))
+
+			ng := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ng.AABB().Copy(*hyperrectangle.New(vector.V([]float64{3, 3}), vector.V([]float64{4, 4})))
+
+			nb := c.GetOrDie(c.Insert(cid.IDInvalid, nd.ID(), ne.ID(), true))
+			nc := c.GetOrDie(c.Insert(cid.IDInvalid, nf.ID(), ng.ID(), true))
+			na := c.GetOrDie(c.Insert(cid.IDInvalid, nb.ID(), nc.ID(), true))
+
+			for _, n := range []node.N{nb, nc, na} {
+				node.SetAABB(n, nil, 1)
+				node.SetHeight(n)
+			}
+
+			return config{
+				name: "Swap",
+				d:    nd,
+				e:    ne,
+				f:    nf,
+				g:    ng,
+				opt:  heuristic.H(nb.AABB().R()) + heuristic.H(nc.AABB().R()),
+				want: w{
+					h:       24,
+					optimal: true,
+				},
+			}
+		}(),
+		func() config {
+			c := cache.New(cache.O{
+				LeafSize: 1,
+				K:        k,
+			})
+
+			nd := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nd.AABB().Copy(*hyperrectangle.New(vector.V([]float64{97, 97}), vector.V([]float64{98, 98})))
+
+			ne := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ne.AABB().Copy(*hyperrectangle.New(vector.V([]float64{99, 99}), vector.V([]float64{100, 100})))
+
+			nf := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nf.AABB().Copy(*hyperrectangle.New(vector.V([]float64{1, 1}), vector.V([]float64{2, 2})))
+
+			ng := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ng.AABB().Copy(*hyperrectangle.New(vector.V([]float64{3, 3}), vector.V([]float64{4, 4})))
+
+			nb := c.GetOrDie(c.Insert(cid.IDInvalid, nd.ID(), ne.ID(), true))
+			nc := c.GetOrDie(c.Insert(cid.IDInvalid, nf.ID(), ng.ID(), true))
+			na := c.GetOrDie(c.Insert(cid.IDInvalid, nb.ID(), nc.ID(), true))
+
+			for _, n := range []node.N{nb, nc, na} {
+				node.SetAABB(n, nil, 1)
+				node.SetHeight(n)
+			}
+
+			return config{
+				name: "NoSwap/AABB",
+				d:    nd,
+				e:    ne,
+				f:    nf,
+				g:    ng,
+				opt:  heuristic.H(nb.AABB().R()) + heuristic.H(nc.AABB().R()),
+				want: w{
+					h:       24,
+					optimal: false,
+				},
+			}
+		}(),
+		func() config {
+			c := cache.New(cache.O{
+				LeafSize: 1,
+				K:        k,
+			})
+
+			nd := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nd.AABB().Copy(*hyperrectangle.New(vector.V([]float64{1, 1}), vector.V([]float64{2, 2})))
+			nd.SetHeight(2)
+
+			ne := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ne.AABB().Copy(*hyperrectangle.New(vector.V([]float64{99, 99}), vector.V([]float64{100, 100})))
+
+			nf := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nf.AABB().Copy(*hyperrectangle.New(vector.V([]float64{97, 97}), vector.V([]float64{98, 98})))
+
+			ng := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ng.AABB().Copy(*hyperrectangle.New(vector.V([]float64{3, 3}), vector.V([]float64{4, 4})))
+			ng.SetHeight(2)
+
+			nb := c.GetOrDie(c.Insert(cid.IDInvalid, nd.ID(), ne.ID(), true))
+			nc := c.GetOrDie(c.Insert(cid.IDInvalid, nf.ID(), ng.ID(), true))
+			na := c.GetOrDie(c.Insert(cid.IDInvalid, nb.ID(), nc.ID(), true))
+
+			for _, n := range []node.N{nb, nc, na} {
+				node.SetAABB(n, nil, 1)
+				node.SetHeight(n)
+			}
+
+			return config{
+				name: "NoSwap/Balance",
+				d:    nd,
+				e:    ne,
+				f:    nf,
+				g:    ng,
+				opt:  heuristic.H(nb.AABB().R()) + heuristic.H(nc.AABB().R()),
+				want: w{
+					h:       776,
+					optimal: false,
+				},
+			}
+		}(),
+		func() config {
+			c := cache.New(cache.O{
+				LeafSize: 1,
+				K:        k,
+			})
+
+			nd := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nd.AABB().Copy(*hyperrectangle.New(vector.V([]float64{1, 1}), vector.V([]float64{2, 2})))
+			nd.SetHeight(2)
+
+			ne := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ne.AABB().Copy(*hyperrectangle.New(vector.V([]float64{99, 99}), vector.V([]float64{100, 100})))
+			ne.SetHeight(2)
+
+			nf := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			nf.AABB().Copy(*hyperrectangle.New(vector.V([]float64{97, 97}), vector.V([]float64{98, 98})))
+
+			ng := c.GetOrDie(c.Insert(cid.IDInvalid, cid.IDInvalid, cid.IDInvalid, true))
+			ng.AABB().Copy(*hyperrectangle.New(vector.V([]float64{3, 3}), vector.V([]float64{4, 4})))
+
+			nb := c.GetOrDie(c.Insert(cid.IDInvalid, nd.ID(), ne.ID(), true))
+			nc := c.GetOrDie(c.Insert(cid.IDInvalid, nf.ID(), ng.ID(), true))
+			na := c.GetOrDie(c.Insert(cid.IDInvalid, nb.ID(), nc.ID(), true))
+
+			for _, n := range []node.N{nb, nc, na} {
+				node.SetAABB(n, nil, 1)
+				node.SetHeight(n)
+			}
+
+			return config{
+				name: "NoSwap/Balance/Child",
+				d:    nd,
+				e:    ne,
+				f:    nf,
+				g:    ng,
+				opt:  heuristic.H(nb.AABB().R()) + heuristic.H(nc.AABB().R()),
+				want: w{
+					h:       776,
+					optimal: false,
+				},
+			}
+		}(),
+	}
+
+	for _, c := range configs {
+		t.Run(c.name, func(t *testing.T) {
+			buf := hyperrectangle.New(
+				vector.V(make([]float64, k)),
+				vector.V(make([]float64, k)),
+			).M()
+			got := &w{}
+			got.h, got.optimal = checkDF(c.d, c.e, c.f, c.g, c.opt, buf)
+			if got.h != c.want.h {
+				t.Errorf("h = %v, want = %v", got.h, c.want.h)
+			}
+			if got.optimal != c.want.optimal {
+				t.Errorf("optimal = %v, want = %v", got.optimal, c.want.optimal)
+			}
+		})
+	}
+}
+
 func TestCheckBF(t *testing.T) {
 	const k = 2
 	type w struct {
