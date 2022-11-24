c5c13e0de80ebeae9d1872a05deb48c22a610fdf
Minke Zhang <minke.zhang@gmail.com>
2022-11-24 02:45:45 -0800

    Add candidate performance tests

--- x/internal/cache/op/candidate/candidate_test.go
+++ x/internal/cache/op/candidate/candidate_test.go
@@ -2,6 +2,7 @@ package candidate
 
 import (
 	"fmt"
+	"math"
 	"runtime"
 	"testing"
 
@@ -14,9 +15,9 @@ import (
 
 var (
 	tests = map[string]C{
-		"Bittner":     Bittner,
+		"Bittner":     bittnerRO,
 		"BrianNoyama": BrianNoyama,
-		"Catto":       Catto,
+		"Catto":       cattoRO,
 		"Guttman":     Guttman,
 	}
 )
@@ -32,15 +33,14 @@ func BenchmarkC(b *testing.B) {
 		name      string
 		generator perf.G
 		aabb      hyperrectangle.R
-		batch     int
 	}
 
 	type config struct {
-		name      string
-		c         C
-		generator perf.G
-		aabb      hyperrectangle.R
-		batch     int
+		name  string
+		c     C
+		cache *cache.C
+		root  node.N
+		aabb  hyperrectangle.R
 	}
 
 	scenarios := []scenario{
@@ -48,60 +48,39 @@ func BenchmarkC(b *testing.B) {
 			name:      "Trivial",
 			generator: perf.Trivial,
 			aabb:      *hyperrectangle.New(vector.V{0, 0}, vector.V{1, 1}),
-			batch:     10000,
 		},
-		{
-			name:      "Balanced/N=10000",
-			generator: func() (*cache.C, node.N) { return perf.Balanced(10000) },
-			aabb:      *hyperrectangle.New(vector.V{10, 10}, vector.V{100, 100}),
-			batch:     500,
-		},
+	}
+	for i := 4; i < 8; i++ {
+		n := int(math.Pow(10, float64(i)))
+		scenarios = append(scenarios, scenario{
+			name:      fmt.Sprintf("Balanced/N=%v", n),
+			generator: func() (*cache.C, node.N) { return perf.Balanced(n) },
+			aabb:      *hyperrectangle.New(vector.V{10, 0}, vector.V{11, 1}),
+		})
 	}
 
 	configs := []config{}
 	for _, s := range scenarios {
-		for label, c := range tests {
+		c, n := func() (*cache.C, node.N) {
+			runtime.MemProfileRate = 0
+			defer func() { runtime.MemProfileRate = 512 * 1024 }()
+			return s.generator()
+		}()
+		for label, f := range tests {
 			configs = append(configs, config{
-				name:      fmt.Sprintf("%v/%v", s.name, label),
-				c:         c,
-				generator: s.generator,
-				aabb:      s.aabb,
-				batch:     s.batch,
+				name:  fmt.Sprintf("%v/%v", s.name, label),
+				c:     f,
+				cache: c,
+				root:  n,
+				aabb:  s.aabb,
 			})
 		}
 	}
 
 	for _, c := range configs {
-		type lookup struct {
-			cache *cache.C
-			root  node.N
-		}
-
-		var ls []lookup
-
 		b.Run(c.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				if i%c.batch == 0 {
-
-					ls = func(n int) []lookup {
-						b.StopTimer()
-						runtime.MemProfileRate = 0
-						defer func() { runtime.MemProfileRate = 512 * 1024 }()
-						defer b.StartTimer()
-
-						ls := make([]lookup, 0, n)
-						for i := 0; i < n; i++ {
-							l := &lookup{}
-							l.cache, l.root = c.generator()
-							ls = append(ls, *l)
-						}
-						return ls
-					}(c.batch)
-
-				}
-
-				l := ls[i%c.batch]
-				c.c(l.cache, l.root, c.aabb)
+				c.c(c.cache, c.root, c.aabb)
 			}
 		})
 	}
