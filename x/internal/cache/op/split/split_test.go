69c510c97a9a4ab6765584e52049e97c308603de
Minke Zhang <minke.zhang@gmail.com>
2022-11-26 01:59:27 -0800

    Fix perf test

--- x/internal/cache/op/split/split_test.go
+++ x/internal/cache/op/split/split_test.go
@@ -3,12 +3,14 @@ package split
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"runtime"
 	"testing"
 
 	"github.com/downflux/go-bvh/x/id"
 	"github.com/downflux/go-bvh/x/internal/cache"
 	"github.com/downflux/go-bvh/x/internal/cache/node"
+	"github.com/downflux/go-bvh/x/internal/heuristic"
 	"github.com/downflux/go-geometry/nd/hyperrectangle"
 	"github.com/downflux/go-geometry/nd/vector"
 
@@ -37,14 +39,16 @@ func BenchmarkS(b *testing.B) {
 
 	data := map[id.ID]hyperrectangle.R{}
 	for i := 0.0; i < math.Pow(2, j); i++ {
-		data[id.ID(100+i)] = *hyperrectangle.New(vector.V{i, 0}, vector.V{i + 1, 1})
+		x := float64(rand.Intn(int(math.Pow(2, j))))
+		y := float64(rand.Intn(int(math.Pow(2, j))))
+		data[id.ID(100+i)] = *hyperrectangle.New(vector.V{x, y}, vector.V{x + 1, y + 1})
 	}
 
 	configs := []config{}
 	for l, s := range tests {
 		// For small leaf sizes, the iteration time is too fast, and the
 		// StopTimer / StartTimer invocations take too long.
-		for i := 3; i < j; i++ {
+		for i := 2; i < j; i++ {
 			n := int(math.Pow(2, float64(i)))
 			configs = append(configs, config{
 				name: fmt.Sprintf("%v/LeafSize=%v", l, n),
@@ -56,12 +60,18 @@ func BenchmarkS(b *testing.B) {
 
 	for _, c := range configs {
 		b.Run(c.name, func(b *testing.B) {
+			// One measure of how effectively nodes are split is the
+			// rough size of the resultant AABB boxes -- ideally,
+			// the boxes should be of equivalent sizes.
+			var diff float64
+			var start float64
 			for i := 0; i < b.N; i++ {
 				ch, n, m := func() (*cache.C, node.N, node.N) {
 					b.StopTimer()
 					runtime.MemProfileRate = 0
 					defer func() { runtime.MemProfileRate = 512 * 1024 }()
 					defer b.StartTimer()
+
 					c := cache.New(cache.O{
 						LeafSize: c.size,
 						K:        k,
@@ -76,10 +86,27 @@ func BenchmarkS(b *testing.B) {
 						n.Leaves()[x] = struct{}{}
 					}
 					node.SetAABB(n, data, 1)
+
+					start = heuristic.H(n.AABB().R())
+
 					return c, n, m
 				}()
 				c.s(ch, data, n, m)
+				func() {
+					b.StopTimer()
+					defer b.StartTimer()
+
+					// Update AABBs of the nodes.
+					node.SetAABB(n, data, 1)
+					node.SetAABB(m, data, 1)
+
+					end := math.Abs(n.Heuristic() - m.Heuristic())
+					diff += end / start
+				}()
 			}
+			// Track the left / right AABB heuristic balance as a
+			// result of the split. Lower is better.
+			b.ReportMetric(diff/float64(b.N)*100, "%")
 		})
 	}
 }
