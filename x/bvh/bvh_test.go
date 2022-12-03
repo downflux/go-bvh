c0c11f3a365ef99a751455c3052762d9323a346c
Minke Zhang <minke.zhang@gmail.com>
2022-12-02 21:16:50 -0800

    Add BVH conformance test for BrianNoyama

--- x/bvh/bvh_test.go
+++ x/bvh/bvh_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/downflux/go-bvh/x/container/briannoyama"
 	"github.com/downflux/go-bvh/x/container/bruteforce"
 	"github.com/downflux/go-bvh/x/id"
 	"github.com/downflux/go-bvh/x/perf"
@@ -14,8 +15,8 @@ import (
 )
 
 func TestBroadPhaseConformance(t *testing.T) {
+	const k = 3
 	type config struct {
-		k         vector.D
 		size      int
 		tolerance float64
 		data      map[id.ID]hyperrectangle.R
@@ -24,95 +25,108 @@ func TestBroadPhaseConformance(t *testing.T) {
 
 	configs := []config{
 		{
-			k:         2,
 			size:      4,
 			tolerance: 1.05,
 			data:      map[id.ID]hyperrectangle.R{},
 			q: *hyperrectangle.New(
-				vector.V([]float64{10, 10}),
-				vector.V([]float64{90, 90}),
+				vector.V([]float64{10, 10, 10}),
+				vector.V([]float64{90, 90, 90}),
 			),
 		},
 		{
-			k:         2,
 			size:      1,
 			tolerance: 1.05,
 			data: map[id.ID]hyperrectangle.R{
 				100: *hyperrectangle.New(
-					vector.V([]float64{0, 0}),
-					vector.V([]float64{1, 1}),
+					vector.V([]float64{0, 0, 0}),
+					vector.V([]float64{1, 1, 1}),
 				),
 				101: *hyperrectangle.New(
-					vector.V([]float64{10, 10}),
-					vector.V([]float64{11, 11}),
+					vector.V([]float64{10, 10, 10}),
+					vector.V([]float64{11, 11, 11}),
 				),
 			},
 			q: *hyperrectangle.New(
-				vector.V([]float64{1, 1}),
-				vector.V([]float64{10, 10}),
+				vector.V([]float64{1, 1, 1}),
+				vector.V([]float64{10, 10, 10}),
 			),
 		},
 		{
-			k:         2,
 			size:      4,
 			tolerance: 1.05,
 			data: map[id.ID]hyperrectangle.R{
 				100: *hyperrectangle.New(
-					vector.V([]float64{0, 0}),
-					vector.V([]float64{1, 1}),
+					vector.V([]float64{0, 0, 0}),
+					vector.V([]float64{1, 1, 1}),
 				),
 				101: *hyperrectangle.New(
-					vector.V([]float64{10, 10}),
-					vector.V([]float64{11, 11}),
+					vector.V([]float64{10, 10, 10}),
+					vector.V([]float64{11, 11, 11}),
 				),
 				102: *hyperrectangle.New(
-					vector.V([]float64{9, 9}),
-					vector.V([]float64{11, 11}),
+					vector.V([]float64{9, 9, 9}),
+					vector.V([]float64{11, 11, 11}),
 				),
 				103: *hyperrectangle.New(
-					vector.V([]float64{30, 30}),
-					vector.V([]float64{40, 40}),
+					vector.V([]float64{30, 30, 30}),
+					vector.V([]float64{40, 40, 40}),
 				),
 				104: *hyperrectangle.New(
-					vector.V([]float64{100, 100}),
-					vector.V([]float64{101, 101}),
+					vector.V([]float64{100, 100, 100}),
+					vector.V([]float64{101, 101, 101}),
 				),
 				105: *hyperrectangle.New(
-					vector.V([]float64{90.01, 90.01}),
-					vector.V([]float64{95, 95}),
+					vector.V([]float64{90.01, 90.01, 90.01}),
+					vector.V([]float64{95, 95, 95}),
 				),
 				106: *hyperrectangle.New(
-					vector.V([]float64{0, 0}),
-					vector.V([]float64{100, 100}),
+					vector.V([]float64{0, 0, 0}),
+					vector.V([]float64{100, 100, 100}),
 				),
 			},
 			q: *hyperrectangle.New(
-				vector.V([]float64{10, 10}),
-				vector.V([]float64{90, 90}),
+				vector.V([]float64{10, 10, 10}),
+				vector.V([]float64{90, 90, 90}),
 			),
 		},
 		{
-			k:         2,
 			size:      1,
 			tolerance: 1.05,
-			data:      perf.GenerateRandomBoxes(10000, 2, 100, 200),
-			q:         perf.GenerateAABB(2, 50, 70),
+			data:      perf.GenerateRandomBoxes(10000, k, 100, 200),
+			q:         perf.GenerateAABB(k, 50, 70),
 		},
 		{
-			k:         2,
 			size:      4,
 			tolerance: 1.05,
-			data:      perf.GenerateRandomBoxes(1000, 2, 100, 200),
-			q:         perf.GenerateAABB(2, 50, 70),
+			data:      perf.GenerateRandomBoxes(1000, k, 100, 200),
+			q:         perf.GenerateAABB(k, 50, 70),
 		},
 	}
 
 	for _, c := range configs {
-		name := fmt.Sprintf("K=%v/LeafSize=%v/Tolerance=%v/N=%v", c.k, c.size, c.tolerance, len(c.data))
-		t.Run(name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("BrianNoyama/K=%v/Tolerance=%v/N=%v", k, c.size, len(c.data)), func(t *testing.T) {
+			tbf := bruteforce.New()
+			tbn := briannoyama.New()
+
+			for x, h := range c.data {
+				tbf.Insert(x, h)
+				tbn.Insert(x, h)
+			}
+
+			want := tbf.BroadPhase(c.q)
+			got := tbn.BroadPhase(c.q)
+
+			if diff := cmp.Diff(
+				want, got,
+				cmpopts.SortSlices(func(a, b id.ID) bool { return a < b }),
+			); diff != "" {
+				t.Errorf("BroadPhase() mismatch (-want +got):\n%v", diff)
+			}
+		})
+		t.Run(fmt.Sprintf("BVH/K=%v/LeafSize=%v/Tolerance=%v/N=%v", k, c.size, c.tolerance, len(c.data)), func(t *testing.T) {
 			tbf := bruteforce.New()
 			tbvh := New(O{
-				K:         c.k,
+				K:         k,
 				LeafSize:  c.size,
 				Tolerance: c.tolerance,
 			})
@@ -122,13 +136,8 @@ func TestBroadPhaseConformance(t *testing.T) {
 				tbvh.Insert(x, h)
 			}
 
-			q := *hyperrectangle.New(
-				vector.V([]float64{10, 10}),
-				vector.V([]float64{90, 90}),
-			)
-
-			want := tbf.BroadPhase(q)
-			got := tbvh.BroadPhase(q)
+			want := tbf.BroadPhase(c.q)
+			got := tbvh.BroadPhase(c.q)
 
 			if diff := cmp.Diff(
 				want, got,
